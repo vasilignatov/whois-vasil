bc09eed7e158856906e99cacb0edd3a7ec97ca09
Vasil Ignatov <vasilignatow@gmail.com>
2025-06-24 22:40:24 +0300

    Re-init project

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  }
-}) 
+  plugins: [react(), tailwindcss()],
+})
